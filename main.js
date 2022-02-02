47dab6e7268cb876075c3f1f6498af06c496efb8
Abbi Engel <abbi.engel33@gmail.com>
2022-02-02 09:37:20 -0600

    driving age2.0

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import input from 'input';
 async function main() {
     const driving = 18
     const myAge = await input.text('What age are you?');
-if (myAge >= driving) {
-    console.log('You can drive!')
-} else {
-    console.log('You can not drive!')
-} 
+    if (myAge >= driving) {
+        console.log('You can drive!')
+    } else {
+        console.log('You can not drive!')
+    } 
 
 }
 
