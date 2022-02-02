e5344fbbaef958b3fbd19b6e3365574dde507bdc
Abbi Engel <abbi.engel33@gmail.com>
2022-02-02 13:58:19 -0600

    third commit

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import input from 'input';
 
+
 async function main() {
-    const driving = 18
-    const myAge = await input.text('What age are you?');
-    const hasLicense = await input.confirm('Do you have your license?');
-    if (myAge >= driving && hasLicense) {
-        console.log('You can drive!')
-    } else 
-        console.log('You can not drive!')
-    if (myAge<driving) {
-        console.log(`Wait ${driving - myAge} years.`)
+    const drivingAge = 18
+    let toContinue = true;
+
+    while (toContinue) { 
+        const age = await input.text('What is your age');
+        const hasLicense = await input.confirm('Do you have your license');
+
+        if(age >= drivingAge && hasLicense) {
+            console.log('They can drive!');
+        } else {
+            if (age < drivingAge) {
+                console.log(`Please wait ${drivingAge - age} years.`);
+        } else {
+                console.log('They need their license!');
+        }
+        }
+        toContinue = await input.text('Do you wish to continue?');
     }
-
 }
-
 main();
